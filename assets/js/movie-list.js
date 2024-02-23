d9834fdbaf908bb3b5f9ef9b6299a7a3eb976f05
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-23 18:11:38 +0200

    Update movie-list.js

--- assets/js/movie-list.js
+++ assets/js/movie-list.js
@@ -14,7 +14,7 @@ sidebar()
 let currentPage = 1
 let totalPages = 0
 
-fetchDataFromServer(`https://api.themoviedb.org/3/discover/movie/?api_key=${api_key}&sort_by=popularity.desc&include_adult=false&page=${currentPage}&${urlParam}`, function ({ results: movieList, total_pages }) {
+fetchDataFromServer(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&include_adult=false&page=${currentPage}&${urlParam}`, function ({ results: movieList, total_pages }) {
 	totalPages = total_pages
 	document.title = genreName ? `${genreName} Movies - Tvflix` : 'Movies - Tvflix'
 	const movieListElement = document.createElement('section')
@@ -49,7 +49,7 @@ fetchDataFromServer(`https://api.themoviedb.org/3/discover/movie/?api_key=${api_
 		currentPage++
 		this.classList.add('loading') // this == loading-btn
 
-		fetchDataFromServer(`https://api.themoviedb.org/3/discover/movie/?api_key=${api_key}&sort_by=popularity.desc&include_adult=false&page=${currentPage}&${urlParam}`, ({ results: movieList }) => {
+		fetchDataFromServer(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&include_adult=false&page=${currentPage}&${urlParam}`, ({ results: movieList }) => {
 			this.classList.remove('loading') // this == loading-btn
 
 			for (const movie of movieList) {
